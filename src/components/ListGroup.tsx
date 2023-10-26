91f0dd29749ad178478b47223447b9405feb3764
Tim Abbott <20151421+timothyabbott@users.noreply.github.com>
2023-10-26 19:16:47 +0200

    15. Handling Events
    Two handlers added, one commented out and left in for reference

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,32 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
     let items = ['New York', 'London', 'Paris', 'Madrid']
-    items = []
-    return (
-        <>
-            <h1>List</h1>
-            <ul className="list-group">
-                {/* this works because JS will return the right most value in any logical and comparison */}
-                {items.length === 0 && <p>No Item</p>}
-                {/* this is JS, we must wrap it in braces 
-                the key is needed so that react can keep track of the elements*/}
-                {items.map(item => <li className="list-group-item" key={item}>{item}</li>)}
+    const handleClick = (event:MouseEvent) => console.log(event) 
+    
 
-            </ul>
-        </>
-    )
+    return (
+      <>
+        <h1>List</h1>
+        <ul className="list-group">
+          {/* this works because JS will return the right most value in any logical and comparison */}
+          {items.length === 0 && <p>No Item</p>}
+          {/* this is JS, we must wrap it in braces 
+                the key is needed so that react can keep track of the elements*/}
+          {items.map((item, index) => (
+            <li
+              className="list-group-item"
+            //   this works but we can also use an external function
+            //   onClick={() => console.log(item, index)}
+            onClick={handleClick}
+              key={item}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
 }
 
 export default ListGroup;
